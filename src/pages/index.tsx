9ad8b38d64211835f881ef04b29ea04d50fc7ccd
Pedro Val <prmval@gmail.com>
2025-03-10 14:10:51 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Login from '@/components/Login';
+
+export default function Home() {
+  return (
+    <div className="bg-slate-300 w-full  min-h-screen">
+      Default Blank Front End Project
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      screens: {
+        xl2: '1350px',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
